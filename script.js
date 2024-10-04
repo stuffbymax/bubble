c233d9de48c4b3d454afd7e06ef6de973e721f6d
stuffbymax <96355058+stuffbymax@users.noreply.github.com>
2024-10-04 19:17:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -43,49 +43,54 @@ function copyCode() {
         }
         </style>
         `;
-    const jsCode = `<script>
-            setInterval(function () {
-                const newBubble = document.createElement('div');
-                newBubble.classList.add('bubbles');
-                document.body.appendChild(newBubble);
-                newBubble.style.top = Math.floor(Math.random() * window.innerHeight + 1) + 'px';
-                newBubble.style.left = Math.floor(Math.random() * window.innerWidth + 1) + 'px';
+  const jsCode = `<script>
+        colorInputBubble.addEventListener('input', updateBubbleColor);
+        colorInputBackground.addEventListener('input', updateBackgroundColor);
 
-                const myDirection = Math.floor(Math.random() * 5);
+        setInterval(function () {
+            const newBubble = document.createElement('div');
+            newBubble.classList.add('bubbles');
+            const colorInputBubble = document.getElementById('color-input-bubble');
+
+            const hexColor = colorInputBubble.value;
+            document.body.appendChild(newBubble);
+            newBubble.style.backgroundColor = hexColor;
+            newBubble.style.top = Math.floor(Math.random() * window.innerHeight + 1) + 'px';
+            newBubble.style.left = Math.floor(Math.random() * window.innerWidth + 1) + 'px';
+
+            const myDirection = Math.floor(Math.random() * 5);
+            setTimeout(function () {
+                newBubble.style.opacity = '0.5';
+            }, 1);
+
+            const speed = ${MIN_MOVEMENT_SPEED + Math.random() * (MAX_MOVEMENT_SPEED - MIN_MOVEMENT_SPEED)};
+            let mytime = setInterval(function () {
+                if (myDirection === 1) {
+                    newBubble.style.top = (parseInt(newBubble.style.top) - speed) + 'px';
+                    newBubble.style.left = (parseInt(newBubble.style.left) - speed) + 'px';
+                } else if (myDirection === 2) {
+                    newBubble.style.top = (parseInt(newBubble.style.top) - speed) + 'px';
+                    newBubble.style.right = (parseInt(newBubble.style.right) - speed) + 'px';
+                } else if (myDirection === 3) {
+                    newBubble.style.top = (parseInt(newBubble.style.top) + speed) + 'px';
+                    newBubble.style.left = (parseInt(newBubble.style.left) + speed) + 'px';
+                } else {
+                    newBubble.style.top = (parseInt(newBubble.style.top) + speed) + 'px';
+                    newBubble.style.right = (parseInt(newBubble.style.right) + speed) + 'px';
+                }
+                if (parseInt(newBubble.style.top) <= 0 || parseInt(newBubble.style.left) <= 0 || parseInt(newBubble.style.right) >= window.innerWidth || parseInt(newBubble.style.bottom) >= window.innerHeight) {
+                    return clearInterval(mytime);
+                }
+            }, 1000 / 60);
+
+            setTimeout(function () {
+                newBubble.classList.add('bubble-hide');
                 setTimeout(function () {
-                    newBubble.style.opacity = '0.5';
-                }, 1);
-
-                const speed = 0.16;
-                let mytime = setInterval(function () {
-                    if (myDirection === 1) {
-                        newBubble.style.top = (parseInt(newBubble.style.top) - speed) + 'px';
-                        newBubble.style.left = (parseInt(newBubble.style.left) - speed) + 'px';
-                    } else if (myDirection === 2) {
-                        newBubble.style.top = (parseInt(newBubble.style.top) - speed) + 'px';
-                        newBubble.style.right = (parseInt(newBubble.style.right) - speed) + 'px';
-                    } else if (myDirection === 3) {
-                        newBubble.style.top = (parseInt(newBubble.style.top) + speed) + 'px';
-                        newBubble.style.left = (parseInt(newBubble.style.left) + speed) + 'px';
-                    } else {
-                        newBubble.style.top = (parseInt(newBubble.style.top) + speed) + 'px';
-                        newBubble.style.right = (parseInt(newBubble.style.right) + speed) + 'px';
-                    }
-                    if (newBubble.style.top <= 0) return clearInterval(mytime);
-                    if (newBubble.style.left <= 0) return clearInterval(mytime);
-                    if (newBubble.style.right >= window.innerWidth) return clearInterval(mytime);
-                    if (newBubble.style.bottom >= window.innerWidth) return clearInterval(mytime); 
-                }, 1000 / 60);
-
-                setTimeout(function () {
-                    newBubble.classList.add('bubble-hide');
-                    setTimeout(function () {
-                        newBubble.remove();
-                    }, 2000);
-                }, Math.floor(Math.random() * 1000) + 2000);
-            }, 1000);
-            </script>`
-
+                    newBubble.remove();
+                }, ${BUBBLE_FADE_OUT_TIME});
+            }, Math.floor(Math.random() * 1000) + ${BUBBLE_FADE_OUT_TIME});
+        }, ${BUBBLE_CREATION_INTERVAL});
+    </script>`;
 
     const codeToCopy = cssCode + jsCode;
 
